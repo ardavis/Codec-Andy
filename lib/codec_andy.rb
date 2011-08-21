0d735ab7b5cc9798b05c71813a268d25e050f0ca
Andrew Davis <andrew@andrewrdavis.net>
2011-08-21 17:36:03 -0400

    Modified codec_andy to run a minimagick test

--- lib/codec_andy.rb
+++ lib/codec_andy.rb
@@ -1,3 +1,9 @@
 require 'rubygems'
 require 'mini_magick'
 
+path = "/Users/ardavis/Pictures/Atlantis/Andy_Crawler.jpg"
+image = MiniMagick::Image.new(path)
+
+#print width and height
+puts image[:width]
+puts image[:height]
