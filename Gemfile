435d875335e1060bf309731baf0ff0df9ac1f661
Andrew Davis <andrew@andrewrdavis.net>
2011-08-23 10:58:12 -0400

    Added a counter to count the number of times for each pixel

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ source "http://rubygems.org"
 # Gems
 gem 'mini_magick', '3.3'
 gem 'rmagick', '2.13.1'
+gem 'ruby-debug19'
 
--- lib/codec_andy/codec_andy.rb
+++ lib/codec_andy/codec_andy.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
-require 'mini_magick'
 require 'RMagick'
+require 'ruby-debug'
 
 include Magick
 
-path = "../../Images/lena512color.tiff"
+# Final Variables
+RED   = 0
+GREEN = 1
+BLUE  = 2
+
+path = "#{File.dirname(__FILE__)}/../../Images/lena512color.tiff"
 image = ImageList.new(path)
 
 # Get the pixel values of the input image
@@ -14,15 +19,34 @@ pixels = image.get_pixels(0, 0, image.columns, image.rows)
 # Initialize an array of columns*rows
 image_array = Array.new(image.columns) { Array.new(image.rows) { Array.new(3) {0} } }
 
+# Initialize empty array of every possible pixel value... (0, 0, 0) => (256, 256, 256)
+pixel_array = Array.new(256) { Array.new(256) { Array.new(256) {0} } }
+
 # Print Dimensions
-puts "Width: " + image.rows.to_s
-puts "Height: " + image.columns.to_s
-puts "# of Pixels: " + pixels.count.to_s
+#puts "Width: " + image.rows.to_s
+#puts "Height: " + image.columns.to_s
+#puts "# of Pixels: " + pixels.count.to_s
 
 # Store pixels into the array
-(0..(image.columns * image.rows)).each do |pixel|
 
+(0..image.rows-1).each do |row|
+  (0..image.columns-1).each do |col|
+    debugger
+    current_pixel = (row * image.rows) + col
+
+    red_value   = pixels[current_pixel].red
+    green_value = pixels[current_pixel].green
+    blue_value  = pixels[current_pixel].blue
+
+    image_array[row][col][RED]    = red_value
+    image_array[row][col][GREEN]  = green_value
+    image_array[row][col][BLUE]   = blue_value
+
+    pixel_array[red_value][green_value][blue_value] = pixel_array[red_value][green_value][blue_value] + 1
+  end
 end
+debugger
+
 
 
 #Array.new(512) { Array.new(512) { Array.new(3) }
